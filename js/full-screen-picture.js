0dcbc0c42c310bcfc7507130e8b8cf161bfb5773
Nikita <runoff.nikita92@gmail.com>
2023-03-14 21:39:31 +0300

    отрисовывает комментарии под фото

--- js/full-screen-picture.js
+++ js/full-screen-picture.js
@@ -1,17 +1,40 @@
 import {pictures} from './thumbnail-rendering.js';
+import {similarPictures} from './thumbnail-rendering.js';
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureImg = document.querySelector('.big-picture__img');
+const socialComments = document.querySelector('.social__comments');
+
+const renderComments = (item) => {
+  //socialComments.innerHTML = ''; //очищает разметку
+  item.forEach(({avatar, name, message}) => {
+    const socialComment = document.querySelector('.social__comment').cloneNode(true);
+
+    socialComment.querySelector('.social__picture').src = avatar;
+    socialComment.querySelector('.social__picture').alt = name;
+    socialComment.querySelector('.social__text').textContent = message;
+
+    socialComments.append(socialComment);
+  });
+
+};
 
 const openBigPicture = (evt) => {
   if (evt.target.closest('.picture')) {
     bigPicture.classList.remove('hidden');
   }
-  bigPictureImg.querySelector('img').src = evt.target.closest('.picture__img').src;
+  const pictureId = Number(evt.target.closest('.picture').querySelector('.picture__img').dataset.id);
+  const pictureItem = similarPictures.find((item) => item.id === pictureId);
+  const commentItems = pictureItem.comments;
+  //console.log(commentItems);
+  //console.log(pictureItem.description);
+
+  bigPictureImg.querySelector('img').src = evt.target.closest('.picture').querySelector('.picture__img').src;
 
   bigPicture.querySelector('.likes-count').textContent = evt.target.closest('.picture').querySelector('.picture__likes').textContent;
-  bigPicture.querySelector('.social__caption').textContent = evt.target.closest('.picture').querySelector('.picture__description').textContent;
+  bigPicture.querySelector('.social__caption').textContent = pictureItem.description;
   bigPicture.querySelector('.comments-count').textContent = evt.target.closest('.picture').querySelector('.picture__comments').textContent;
 
+  renderComments(commentItems); //добавляет комменты в разметку
 };
 
 pictures.addEventListener('click', openBigPicture); //открывает фотку
@@ -24,6 +47,6 @@ const closeBigPicture = (evt) => {
 
 bigPicture.addEventListener('click', closeBigPicture); //закрывает фотку
 
-const socialComment = () => document.querySelector('.social__comment').cloneNode(true);
+//const socialComment = () => document.querySelector('.social__comment').cloneNode(true);
 
-console.log(socialComment());
+//console.log(socialComment());
--- js/thumbnail-rendering.js
+++ js/thumbnail-rendering.js
@@ -10,17 +10,19 @@ const similarPictures = createPictures();
 
 const similarListFragment = document.createDocumentFragment();
 
-similarPictures.forEach(({url, likes, comments}) => {
+similarPictures.forEach(({url, likes, comments, id}) => {
   const pictureElement = picture.cloneNode(true);
-
+//console.log(id);
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__likes').textContent = likes;
   pictureElement.querySelector('.picture__comments').textContent = comments.length;
-
+  pictureElement.querySelector('.picture__img').setAttribute('data-id', id);
   similarListFragment.appendChild(pictureElement);
 });
 
 pictures.appendChild(similarListFragment);
 
 export{pictures};
+export{similarPictures};
+
 
