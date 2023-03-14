3394abeee2be07e28ce99e640e93a58607219ace
Nikita <runoff.nikita92@gmail.com>
2023-03-14 15:37:06 +0300

    добавляет функции открыть закрыть

--- js/full-screen-picture.js
+++ js/full-screen-picture.js
@@ -1,13 +1,29 @@
 import {pictures} from './thumbnail-rendering.js';
 const bigPicture = document.querySelector('.big-picture');
+const bigPictureImg = document.querySelector('.big-picture__img');
 
-function openBigPicture (evt) {
+const openBigPicture = (evt) => {
   if (evt.target.closest('.picture')) {
     bigPicture.classList.remove('hidden');
   }
+  bigPictureImg.querySelector('img').src = evt.target.closest('.picture__img').src;
 
-}
+  bigPicture.querySelector('.likes-count').textContent = evt.target.closest('.picture').querySelector('.picture__likes').textContent;
+  bigPicture.querySelector('.social__caption').textContent = evt.target.closest('.picture').querySelector('.picture__description').textContent;
+  bigPicture.querySelector('.comments-count').textContent = evt.target.closest('.picture').querySelector('.picture__comments').textContent;
 
-pictures.addEventListener('click', openBigPicture);
+};
 
+pictures.addEventListener('click', openBigPicture); //открывает фотку
 
+const closeBigPicture = (evt) => {
+  if(evt.target.closest('.big-picture__cancel')) {
+    bigPicture.classList.add('hidden');
+  }
+};
+
+bigPicture.addEventListener('click', closeBigPicture); //закрывает фотку
+
+const socialComment = () => document.querySelector('.social__comment').cloneNode(true);
+
+console.log(socialComment());
--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@ import {createPictures} from './data.js';
 import './thumbnail-rendering.js';
 import './full-screen-picture.js';
 
-createPictures();
+console.log(createPictures());
