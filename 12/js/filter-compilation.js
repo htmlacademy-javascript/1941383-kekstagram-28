0abfef8cf62f454776c39fc0be826d62d8f6eee6
Keks <keks@htmlacademy.ru>
2023-04-08 00:42:28 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/filter-compilation.js
@@ -0,0 +1,43 @@
+
+const PHOTO_COUNT = 10;
+
+const imgFilters = document.querySelector('.img-filters');
+const filterRandom = imgFilters.querySelector('#filter-random');
+const filterDefault = imgFilters.querySelector('#filter-default');
+const filterDiscussed = imgFilters.querySelector('#filter-discussed');
+
+const randomPictures = (data) => data.sort(() => Math.random() - 0.5).slice(0, PHOTO_COUNT);
+
+const sortDescending = (data, key) => data.sort((user1, user2) => user2[key] > user1[key] ? 1 : -1);
+
+
+const chooseFilter = (filterType) => {
+  filterDefault.classList.remove('img-filters__button--active');
+  filterDiscussed.classList.remove('img-filters__button--active');
+  filterRandom.classList.remove('img-filters__button--active');
+  filterType.classList.add('img-filters__button--active');
+};
+
+const getRandomPhoto = (cb) => {
+  filterRandom.addEventListener('click', (evt) => {
+    chooseFilter(evt.target);
+    cb();
+  });
+};
+
+const getDefaultPhoto = (cb) => {
+  filterDefault.addEventListener('click', (evt) => {
+    chooseFilter(evt.target);
+    cb();
+  });
+};
+
+const getDiscussedPhoto = (cb) => {
+  filterDiscussed.addEventListener('click', (evt) => {
+    chooseFilter(evt.target);
+    cb();
+  });
+};
+
+
+export {imgFilters,getRandomPhoto, randomPictures, getDefaultPhoto, sortDescending, getDiscussedPhoto};
---
+++ 12/js/main.js
@@ -0,0 +1,33 @@
+
+import './full-screen-picture.js';
+import './form.js';
+import './filters.js';
+import './scale.js';
+import './filter-compilation.js';
+import {renderThumbnails, debounceRenderGallery} from './thumbnail-rendering.js';
+import {getData} from './api.js';
+import {showAlert} from './util.js';
+import {imgFilters, getRandomPhoto,randomPictures, getDefaultPhoto, getDiscussedPhoto, sortDescending} from './filter-compilation.js';
+import {setFormSubmit, closeUserModal, showMessageOnSuccess, showErrorMessage} from './form.js';
+
+let similarPictures = '';
+const sortKey = 'comments';
+
+setFormSubmit(closeUserModal,showMessageOnSuccess, showErrorMessage);
+
+getData()
+  .then((objects) => {
+    similarPictures = objects;
+    imgFilters.classList.remove('img-filters--inactive');
+    renderThumbnails(similarPictures);
+    getDefaultPhoto(() => debounceRenderGallery(similarPictures.slice()));
+    getRandomPhoto(() => debounceRenderGallery(randomPictures(similarPictures.slice())));
+    getDiscussedPhoto(() => debounceRenderGallery(sortDescending(similarPictures.slice(), sortKey)));
+  })
+  .catch(
+    (err) => {
+      showAlert(err.message);
+    }
+  );
+
+export{similarPictures};
