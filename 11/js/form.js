73c292b82dc03b5011149393ef7179512d70b328
Keks <keks@htmlacademy.ru>
2023-04-07 16:33:02 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/form.js
@@ -0,0 +1,175 @@
+import {body} from './full-screen-picture.js';
+import {isEscapeKey} from './util.js';
+import {resetEffects} from './filters.js';
+import {imagePreview} from './filters.js';
+import {validateHashTag} from './validation.js';
+import {getOnlyNumber} from './util.js';
+import {sendData} from './api.js';
+
+const SCALE_VALUE = `${100}%`;
+
+const imgUploadOverlay = document.querySelector('.img-upload__overlay');
+const imgUploadInput = document.querySelector('.img-upload__input');
+const imgUploadCancel = document.querySelector('.img-upload__cancel');
+const uploadFile = document.querySelector('#upload-file');
+const imgUploadForm = document.querySelector('.img-upload__form');
+const hashTags = imgUploadForm.querySelector('#hashtags');
+const textDescription = document.querySelector('.text__description');
+const submitButton = document.querySelector('.img-upload__submit');
+const scaleControlValue = document.querySelector('.scale__control--value');
+const error = document.querySelector('#error').content.querySelector('.error');
+
+const SubmitButtonText = {
+  IDLE: 'Опубликовать',
+  SENDING: 'Публикую...'
+};
+
+const blockSubmitButton = () => {
+  submitButton.disabled = true;
+  submitButton.textContent = SubmitButtonText.SENDING;
+};
+
+const unblockSubmitButton = () => {
+  submitButton.disabled = false;
+  submitButton.textContent = SubmitButtonText.IDLE;
+};
+
+const hiddenUserModal = () => {
+  imgUploadOverlay.classList.add('hidden');
+};
+
+const closeUserModal = () => {
+  imgUploadOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  uploadFile.value = '';
+  hashTags.value = '';
+  textDescription.value = '';
+  scaleControlValue.value = '';
+  resetEffects();
+};
+
+const showhiddenUserModal = () => {
+  imgUploadOverlay.classList.remove('hidden');
+};
+
+const onCloseUploadKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeUserModal();
+  }
+};
+
+const closeUploadPicture = (evt) => {
+  if(evt.target.closest('.img-upload__cancel')) {
+    closeUserModal();
+  }
+  document.removeEventListener('keydown', onCloseUploadKeydown);
+};
+
+const onStopEsc = (inputName) => {
+  inputName.addEventListener('keydown', (evt) =>{
+    if(inputName === document.activeElement){
+      if (evt.key === 'Escape') {
+        evt.stopPropagation();
+      }
+    }
+  });
+};
+
+const closeMessageSuccess = () => {
+  const sectionSuccess = document.querySelector('.success');
+  sectionSuccess.remove();
+};
+
+const closeMessageSuccessKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeMessageSuccess();
+  }
+};
+
+const closeMessageSuccessAnyClick = (evt) => {
+  if(evt.target.closest('.success') && !evt.target.closest('.success__inner')) {
+    closeMessageSuccess();
+  }
+};
+
+const showMessageOnSuccess = () => {
+  const success = document.querySelector('#success').content.querySelector('.success');
+  const successTemp = success.cloneNode(true);
+  const successButton = successTemp.querySelector('.success__button');
+  successButton.addEventListener('click', closeMessageSuccess);
+  document.addEventListener('keydown', closeMessageSuccessKeydown);
+  successTemp.addEventListener('click', closeMessageSuccessAnyClick);
+  body.appendChild(successTemp);
+};
+
+const closeErrorMessage = () => {
+  const sectionError = document.querySelector('.error');
+  sectionError.remove();
+  showhiddenUserModal();
+};
+
+const closeErrorMessageOnKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    document.addEventListener('keydown', onCloseUploadKeydown);
+    closeErrorMessage();
+  }
+};
+
+const closeErrorMessageOnAnyClick = (evt) => {
+  if(evt.target.closest('.error') && !evt.target.closest('.error__inner')) {
+    closeErrorMessage();
+  }
+};
+
+const showErrorMessage = () => {
+  const errorTemp = error.cloneNode(true);
+  const errorButton = errorTemp.querySelector('.error__button');
+  errorButton.addEventListener('click', closeErrorMessage);
+  document.removeEventListener('keydown', onCloseUploadKeydown);
+  document.addEventListener('keydown', closeErrorMessageOnKeydown);
+  errorTemp.addEventListener('click', closeErrorMessageOnAnyClick);
+  hiddenUserModal();
+  document.body.appendChild(errorTemp);
+};
+
+const pristine = new Pristine(imgUploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'form__item--invalid',
+  successClass: 'form__item--valid',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__error-text',
+});
+
+const setFormSubmit = (onSuccess, showMessage, showError) => {
+  imgUploadForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    if(pristine.validate()) {
+      blockSubmitButton();
+      sendData(new FormData(evt.target))
+        .then(onSuccess)
+        .then(showMessage)
+        .catch(showError)
+        .finally(unblockSubmitButton);
+    }
+  });
+};
+
+imgUploadInput.addEventListener('change', () => {
+  imgUploadOverlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onCloseUploadKeydown);
+  scaleControlValue.value = SCALE_VALUE;
+  imagePreview.style.transform = `scale(${getOnlyNumber(scaleControlValue.value) / 100})`;
+});
+
+onStopEsc(hashTags);
+onStopEsc(textDescription);
+
+imgUploadCancel.addEventListener('click', closeUploadPicture);
+
+pristine.addValidator(hashTags, validateHashTag, 'Ошибка в написании хештега');
+
+export {SCALE_VALUE, scaleControlValue,setFormSubmit,closeUserModal, showMessageOnSuccess, closeMessageSuccess, showErrorMessage};
---
+++ 11/js/thumbnail-rendering.js
@@ -0,0 +1,29 @@
+import {debounce} from './util.js';
+const RERENDER_DELAY = 500;
+
+
+const pictures = document.querySelector('.pictures'); //сюда вставляем
+
+const renderThumbnails = (picturesArray) => {
+  const picture = document.querySelector('#picture').content.querySelector('.picture'); //шаблон который копируем
+
+  const similarListFragment = document.createDocumentFragment();
+
+  picturesArray.forEach(({url, likes, comments, id}) => {
+    const pictureElement = picture.cloneNode(true);
+
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+    pictureElement.querySelector('.picture__img').setAttribute('data-id', id);
+    similarListFragment.appendChild(pictureElement);
+  });
+
+  const pictureArray = pictures.querySelectorAll('.picture');
+  pictureArray.forEach((element) => element.remove());
+  pictures.appendChild(similarListFragment);
+};
+
+const debounceRenderGallery = (data) => debounce(() => renderThumbnails(data), RERENDER_DELAY)();
+
+export{renderThumbnails, pictures, debounceRenderGallery};
